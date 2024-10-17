fb671ffba0163c981acf68b3399d0f1eff263856
Sqwonchy <ilianos321@gmail.com>
2024-10-17 21:24:40 +0300

    Add files via upload

--- module_9_3.py
+++ module_9_3.py
@@ -3,7 +3,7 @@ second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(x[0]) - len(x[1]) for x in zip(first, second) if len(x[0]) - len(x[1]))
 
-second_result = (len(first[x]) == len(second[y]) for x in range(len(first)) for y in range(len(second)) if y == x)
+second_result = (len(first[x]) == len(second[x]) for x in range(len(first)))
 
 if __name__ == "__main__":
     print(list(first_result))
