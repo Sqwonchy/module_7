5747e19abb559de335d214ae502fda1159b4b73b
Sqwonchy <ilianos321@gmail.com>
2024-11-06 22:26:04 +0300

    Add files via upload

---
+++ module_12_2.py
@@ -0,0 +1,42 @@
+import unittest
+from runner_and_tournament import Runner, Tournament
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.usain = Runner("Усейн", 10)
+        self.andrei = Runner("Андрей", 9)
+        self.nik = Runner("Ник", 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for key, value in cls.all_results.items():
+            print(value)
+
+    def test_tournament_1(self):
+        number = 1
+        tour = Tournament(90, self.usain, self.nik)
+        finisher = tour.start()
+        self.all_results = {place: runner.name for place, runner in finisher.items()}
+        self.assertEqual(self.all_results[max(self.all_results)], "Ник")
+        TournamentTest.all_results[number] = self.all_results
+
+    def test_tournament_2(self):
+        number = 2
+        tour = Tournament(90, self.andrei, self.nik)
+        finisher = tour.start()
+        self.all_results = {place: runner.name for place, runner in finisher.items()}
+        self.assertEqual(self.all_results[max(self.all_results)], "Ник")
+        TournamentTest.all_results[number] = self.all_results
+
+    def test_tournament_3(self):
+        number = 3
+        tour = Tournament(90, self.usain, self.andrei, self.nik)
+        finisher = tour.start()
+        self.all_results = {place: runner.name for place, runner in finisher.items()}
+        self.assertEqual(self.all_results[max(self.all_results)], "Ник")
+        TournamentTest.all_results[number] = self.all_results
