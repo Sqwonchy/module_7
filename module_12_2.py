571ebf5a9a4191dd5be0fecd60c6050a87f266cc
Sqwonchy <ilianos321@gmail.com>
2024-11-07 19:15:50 +0300

    Add files via upload

--- module_12_2.py
+++ module_12_2.py
@@ -1,11 +1,13 @@
 import unittest
 from runner_and_tournament import Runner, Tournament
+from unittest import skipIf
 
 
 class TournamentTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
+        is_frozen = True
 
     def setUp(self):
         self.usain = Runner("Усейн", 10)
@@ -17,6 +19,7 @@ class TournamentTest(unittest.TestCase):
         for key, value in cls.all_results.items():
             print(value)
 
+    @skipIf(lambda: TournamentTest.is_frozen, "Тесты в этом кейсе заморожены")
     def test_tournament_1(self):
         number = 1
         tour = Tournament(90, self.usain, self.nik)
@@ -25,6 +28,7 @@ class TournamentTest(unittest.TestCase):
         self.assertEqual(self.all_results[max(self.all_results)], "Ник")
         TournamentTest.all_results[number] = self.all_results
 
+    @skipIf(lambda: TournamentTest.is_frozen, "Тесты в этом кейсе заморожены")
     def test_tournament_2(self):
         number = 2
         tour = Tournament(90, self.andrei, self.nik)
@@ -33,6 +37,7 @@ class TournamentTest(unittest.TestCase):
         self.assertEqual(self.all_results[max(self.all_results)], "Ник")
         TournamentTest.all_results[number] = self.all_results
 
+    @skipIf(lambda: TournamentTest.is_frozen, "Тесты в этом кейсе заморожены")
     def test_tournament_3(self):
         number = 3
         tour = Tournament(90, self.usain, self.andrei, self.nik)
